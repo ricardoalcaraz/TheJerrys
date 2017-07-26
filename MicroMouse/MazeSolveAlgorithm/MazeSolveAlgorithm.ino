cdc36c876a1759da7f9f51c76f8ad70e1aa5cac2
Rosswell <rosswelltiongco@gmail.com>
2017-07-25 22:11:47 -0700

    Wrote additional code
    Got most of the logic for solving the maze and decision process written. Have not been able to test. Left FIXME comments for more experienced programmers to have a look at.

--- MicroMouse/MazeSolveAlgorithm/MazeSolveAlgorithm.ino
+++ MicroMouse/MazeSolveAlgorithm/MazeSolveAlgorithm.ino
@@ -1,8 +1,10 @@
 //An algorithm to solve a maze using the left hand rule that optimizes a path
 int switchPins[4] = {6, 7, 8 , 9}; //Declaring pins for the switches
 int switches;  //Initializing switches
+int stepCount = 0; //Record number of steps taken to determine if goal is reached
+int wallDistance = 5; //Setting threshold to determine if there is a wall or opening
 
-String pathTraveled[i]; //FIXME: Does this work?? We will want to append letters to it.
+String directions;
 void setup() {
   Serial.begin(9600); //Initialize serial communication
 }
@@ -16,7 +18,7 @@ void loop() {
 
 //Constantly reading value of switches and resetting to zero.
 //INPUT: Four Switch pins
-//Returns the value of switches from 0-15
+//OUTPUT: Returns the value of switches from 0-15
 void readSwitches() {
   switches = 0; //Must constantly reset switch count, otherwise it reaches rediculously large numbers
   for (int i = 0; i < 4; i++) {
@@ -33,73 +35,98 @@ void readSwitches() {
       break;
     case B0010: //Machine will solve maze with the optimized path
       Serial.println("Optimized solving maze");
+      solveOptimized();
       break;
     case B0100:
       Serial.println("Optimized solving maze at double speed");
-	  motor1.speed(300);
-	  motor2.speed(300);
-	  solveMaze();
-	  //return motors to normal speed.
-	  motor1.speed(150);
-	  motor2.speed(150); 
+//	  motor1.speed(300);
+//	  motor2.speed(300);
+//	  solveOptimized();
+//	  //return motors to normal speed.
+//	  motor1.speed(150);
+//	  motor2.speed(150); 
       break;
   }
   delay(10); //Stability pause
 }
-
+//INPUT: None
+//OUTPUT: None
+//Purpose: Function solves the maze using left hand rule
 void solveMaze() {
-	foundGoal = False;
-	while foundGoal = False; //Keep trying to solve the maze until we have found the goal
-		if checkIntersection() = "L";
-			turnLeft();
-			//pathTraveled.append("L");
-		else if checkIntersection() = "S"
-			forward();
-			//pathtraveled.append("S");
-		else if checkIntersection() = "R"
-			turnRight();
-			//pathTraveled.append("R");
-		else if checkIntersection() = "B"
-			backward();
-			//pathTraveled.append("B");
-		else //Constanty travel forward until sensors detect a sensor
-			forward(); //Does not count as a 'step' in path
-		optimizePath() //Constantly optimize path after ever intersection
+  while foundGoal() = False{ //Keep trying to solve the maze until we have found the goal
+    //If sensors say it can turn left, turn left and add "L" to the directions
+    if (checkIntersection() = "L"){
+      turnLeft();
+      directions += "L";
+		}
+    //If sensors  can move forward, but cannot move left, continue going forward and add "S" to directions. It is important to add "S" here.
+    else if (checkIntersection() = "S"){
+      forward();
+      directions += "S";
+		}
+    //If motor can only turn right, then turn right and record it.
+    else if (checkIntersection() = "R"){
+      turnRight();
+      directions += "R";
+	  }
+    //If motor can only make a u-turn, do so and record it.
+    else if (checkIntersection() = "B"){
+      backward();
+      directions += "B";
+		}
+    //Mouse should constantly be going forward unless sensors let mouse know it can turn. It is important to never add a direction unless at an intersection
+    else{
+      forward();
+		}
+    optimizePath() //Constantly optimize path after ever intersection
+	}
 }
 
-void solveOptimized(optimizedpath){
-	checkIntersection
+void solveOptimized(directions){
+
+  //Keep going forward, if at an intersection, do the upcoming element turn.
+//	checkIntersection
 }
 
-void optimizePath() {
-	//Execeutes after every move, should be in solveMaze()
-	//LBR = B
-	//LBS = R
-	//RBL = B
-	//SBL = R
-	//SBS = B
-	//LBL = S
-	//if any of these are in pathtraveled:
-		//replace the 3 letters with shorter letter
-
+void optimizePath(directions) {
+  String shortcuts[10] = {"LBR","LBS","RBL","SBL","SBS","LBL"}; //Notice how all the optimizations have "B" in the middle
+  for (shortcuts[i]; shortcuts[i] <= 11; i++){
+    if (i in directions){
+      //FIXME: replace these shrotcuts
+      //LBR = B
+      //LBS = R
+      //RBL = B
+      //SBL = R
+      //SBS = B
+      //LBL = S
+    }
+  }
 }
 
 
-bool foundGoal(){
-  //Read sensor
-  //if either left or right sensor has a reading greater than 10cm for more than 1ms, (subject to change and experimentation),
-	  //turn off motors 1 and 2 to stop moving
-	  //return true
-
+bool foundGoal(){}
+  stepCount = 0; //Constantly resetting the stepper counter
+  //FIXME: if either sensor reading on left or right is greater than normal AND stepCount reaches something like 500 steps, then turn off motors to kill engine
+    //FIXME: Add steps to stepCount in forward();
+    //motor1.turnOff();
+    //motor2.turnOff();
+    //return true
 }
 
-//DOES NOT append paths to array, just check and turn
+//Input: None
+//Output: Returns a letter given sensors
+//DOES NOT append paths to array, just check and turn. We don't want it to append anything otherwise solveOptimized() would constantly add letters
 String checkIntersection(){
-	//Reads sensors
-	//If left sensor is open:
-		// turnLeft(); Add "L" to path
-	//else if front sensor is open:
-		// forward() and add "S" to path
-	// else if right sensor is open:
-		// turnRight() and add "R" to path array
-
+  if (pulseLeft() > wallDistance){
+    return "L";
+  }
+  else if (pulseMiddle() > wallDistance){
+    return "S";
+  }
+  else if (pulseRight() > wallDistance){
+    return "R";
+  }
+  else if (pulseLeft() > wallDistance) and (pulseMiddle() > wallDistance) and (pulseRight() > wallDistance){
+    return "B";
+  }
+}
