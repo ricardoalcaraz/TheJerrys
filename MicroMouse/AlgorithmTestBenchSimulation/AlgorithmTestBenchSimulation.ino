3c71c58b538aa08c5eada57f2170ac1f84c14f6c
Rosswell <rosswelltiongco@gmail.com>
2017-07-27 16:55:17 -0700

    Wrote a testbench for algorithm turning
    Program shows that it can optimize the path

--- MicroMouse/AlgorithmTestBenchSimulation/AlgorithmTestBenchSimulation.ino
+++ MicroMouse/AlgorithmTestBenchSimulation/AlgorithmTestBenchSimulation.ino
@@ -1,9 +1,61 @@
-void setup() {
-  // put your setup code here, to run once:
+String directions;
 
+void setup() {
+  Serial.begin(9600);
+
+  Serial.println("-----SOLVING FIRST TIME-------");
+  Serial.println("turn L");
+  directions.concat('L');
+  directions = optimize(directions);
+  Serial.println(directions);
+
+  Serial.println("turn U");
+  directions.concat('U');
+  directions = optimize(directions);
+  Serial.println(directions);
+  
+  Serial.println("turn L");
+  directions.concat('L');
+  directions = optimize(directions);
+  Serial.println(directions);
+  
+  Serial.println("turn U");
+  directions.concat('U');
+  directions = optimize(directions);
+  Serial.println(directions);
+  
+  Serial.println("turn L");
+  directions.concat('L');
+  directions = optimize(directions);
+  Serial.println(directions);
+  
+  Serial.println("Turn R");
+  directions.concat('R');
+  directions = optimize(directions);
+  Serial.println(directions);
+
+
+  Serial.println("-------OPTIMIZED SOLVE------");
+  recall(directions);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
 }
+
+String optimize(String directions) {
+    String longer[10] = {"LUR","LUS","LUL","RUL","SUL","SUS"};
+    String shorter[10] = {"U","R","S","U","R","U"};
+    for (int count = 0; count <= sizeof(longer); count++){
+      if (directions.endsWith(longer[count])){
+        directions.replace(longer[count],shorter[count]);
+      }
+    }
+  return directions;
+}
+
+void recall(String directions) {
+  for (int i = 0; i <= directions.length(); i++) {
+    Serial.println(directions[i]);
+  }
+}
+
