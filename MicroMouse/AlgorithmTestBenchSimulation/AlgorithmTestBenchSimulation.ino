ce83ef5ceba8b5b39ac8f2bdf8a1726ee30e02eb
Rosswell <rosswelltiongco@gmail.com>
2017-11-01 19:42:43 -0700

    Testbenched method to check if mouse found goal

--- MicroMouse/AlgorithmTestBenchSimulation/AlgorithmTestBenchSimulation.ino
+++ MicroMouse/AlgorithmTestBenchSimulation/AlgorithmTestBenchSimulation.ino
@@ -1,5 +1,5 @@
 String directions;
-
+int stepCounter;
 void setup() {
   Serial.begin(9600);
 
@@ -8,6 +8,7 @@ void setup() {
   directions.concat('L');
   directions = optimize(directions);
   Serial.println(directions);
+  isGoal(directions,stepCounter);
 
   Serial.println("turn U");
   directions.concat('U');
@@ -18,21 +19,39 @@ void setup() {
   directions.concat('L');
   directions = optimize(directions);
   Serial.println(directions);
+  isGoal(directions,stepCounter);
   
   Serial.println("turn U");
   directions.concat('U');
   directions = optimize(directions);
   Serial.println(directions);
+  isGoal(directions,stepCounter);
   
   Serial.println("turn L");
   directions.concat('L');
   directions = optimize(directions);
   Serial.println(directions);
+  isGoal(directions,stepCounter);
   
   Serial.println("Turn R");
   directions.concat('R');
   directions = optimize(directions);
   Serial.println(directions);
+  isGoal(directions,stepCounter);
+
+  Serial.println("Turn R");
+  directions.concat('R');
+  directions = optimize(directions);
+  Serial.println(directions);
+  isGoal(directions,stepCounter);
+
+
+  Serial.println("Turn R");
+  directions.concat('R');
+  directions = optimize(directions);
+  Serial.println(directions);
+  isGoal(directions,stepCounter);
+
 
 
   Serial.println("-------OPTIMIZED SOLVE------");
@@ -57,5 +76,17 @@ void recall(String directions) {
   for (int i = 0; i <= directions.length(); i++) {
     Serial.println(directions[i]);
   }
+}
+
+bool isGoal(String directions, int stepCount){
+  if (directions.endsWith("RRR") & stepCount < 150){
+    Serial.println("Found Goal");
+    return true;
+  }
+  else{
+    Serial.println("No Goal");
+    return false;
+  }
+
 }
 
