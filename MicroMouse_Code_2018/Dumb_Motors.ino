a72e7954ff40793c468f194f5ecc7d154cb1b746
JaapdeDood <jaapdood@gmail.com>
2018-06-08 15:52:28 -0700

    Added tank turns + some comments

--- MicroMouse_Code_2018/Dumb_Motors.ino
+++ MicroMouse_Code_2018/Dumb_Motors.ino
@@ -1,6 +1,6 @@
 //FIXME: make class later
-const unsigned int STEP1 = 4;
-const unsigned int STEP2 = 5;
+const unsigned int STEP1 = 4; // Right Motor
+const unsigned int STEP2 = 5; // Left Motor
 const unsigned int DIR1 = 6;
 const unsigned int DIR2 = 7;
 
@@ -29,3 +29,22 @@ void move_backward(int steps){
         delay(5);
     }
 }
+void turn_tankleft(int steps){
+    digitalWrite(DIR1, HIGH);
+    digitalWrite(DIR2, HIGH);
+    for (int i=0; i<steps; i++){
+        digitalWrite( STEP1, digitalRead(STEP1) ^ 1 );
+        digitalWrite( STEP1, digitalRead(STEP2) ^ 1 );
+        delay(5) ;
+    }    
+}
+void turn_tankright(int steps){
+    digitalWrite(DIR1, LOW);
+    digitalWrite(DIR2, LOW);
+    for (int i=0; i<steps; i++){
+        digitalWrite( STEP1, digitalRead(STEP1) ^ 1 );
+        digitalWrite( STEP1, digitalRead(STEP2) ^ 1 );
+        delay(5) ;
+    }    
+}
+// Use one of the tank turns with an increased amount of steps to U-turn (?)
